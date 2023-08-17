ed1f8c6a69ca60ab9d797a679b14b0f3391cdb38
krystianklimczak <krystianklimczak1996r.com>
2023-08-17 18:34:51 +0200

    refactory gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryArray = [];
 
-console.log(galleryItems);
+galleryItems.forEach(img => {
+  const galleryElement = document.createElement('li');
+  //   galleryElement.classList.add('gallery__item');
+  const imageContent = `
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      alt="${img.description}"
+      
+    />
+  </a>
+`;
+  galleryElement.insertAdjacentHTML('afterbegin', imageContent);
+  galleryArray.push(galleryElement);
+});
+
+gallery.append(...galleryArray);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
