93b2ea56821df2aa0e48e5b1d96c9d5e6a029627
krystianklimczak <krystianklimczak1996r.com>
2023-08-19 22:15:44 +0200

    add video current time function

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
-console.log('123');
+import Vimeo from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+let _ = require('lodash');
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+player.on(
+  'timeupdate',
+  _.throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
