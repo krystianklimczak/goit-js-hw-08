70decd6bb579b280719bb265868b1bf7ce0a09fa
krystianklimczak <krystianklimczak1996r.com>
2023-08-20 14:28:09 +0200

    bugfix to simplebox ex 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,47 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-const gallery = document.querySelector('.gallery');
-const galleryArray = [];
 
-galleryItems.forEach(img => {
+const gallery = document.querySelector('.gallery');
+
+// FIRST METHOD
+
+// const galleryArray = [];
+// galleryItems.forEach(img => {
+//   const galleryElement = document.createElement('li');
+//   const imageContent = `
+//   <a class="gallery__link" href="${img.original}">
+//     <img
+//       class="gallery__image"
+//       src="${img.preview}"
+//       alt="${img.description}"
+
+//     />
+//   </a>
+// `;
+//   galleryElement.insertAdjacentHTML('afterbegin', imageContent);
+//   galleryArray.push(galleryElement);
+// });
+
+// SECOND METHOD
+
+const galleryArray = galleryItems.map(item => {
   const galleryElement = document.createElement('li');
-  //   galleryElement.classList.add('gallery__item');
-  const imageContent = `
-  <a class="gallery__link" href="${img.original}">
-    <img
-      class="gallery__image"
-      src="${img.preview}"
-      alt="${img.description}"
-      
-    />
-  </a>
-`;
+  const imageContent = `<a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+  
+       />
+     </a>`;
   galleryElement.insertAdjacentHTML('afterbegin', imageContent);
-  galleryArray.push(galleryElement);
+  return galleryElement;
 });
 
 gallery.append(...galleryArray);
 
 let lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
   captionsData: 'alt',
   captionDelay: 250,
 });
